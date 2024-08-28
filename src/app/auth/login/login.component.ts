4e6e0147e2e42e96fe8c29d1bf5dedd40333006c
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-28 23:42:28 +0300

    feat(template-driven): extract user input values using NgForm

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [FormsModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    const enteredEmail = formData.value.email;
+    const enteredPassword = formData.value.password;
+
+    console.log('Email: ' + enteredEmail);
+    console.log('Password: ' + enteredPassword);
+  }
+}
