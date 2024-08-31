8e0e0bf21f9f62b2df99f33acfc84fd05f595863
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-31 17:16:16 +0300

    feat(reactive-form): work with nested form groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -27,25 +27,31 @@ export class SignupComponent implements OnInit {
         validators: [Validators.required, Validators.email],
       }),
 
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
+      passwords: new FormGroup({
+        password: new FormControl('', {
+          validators: [Validators.required, Validators.minLength(6)],
+        }),
+
+        confirmPassword: new FormControl('', {
+          validators: [Validators.required],
+        }),
       }),
 
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required],
+      fullName: new FormGroup({
+        firstName: new FormControl('', { validators: [Validators.required] }),
+
+        lastName: new FormControl('', { validators: [Validators.required] }),
       }),
 
-      firstName: new FormControl('', { validators: [Validators.required] }),
+      address: new FormGroup({
+        street: new FormControl('', { validators: [Validators.required] }),
 
-      lastName: new FormControl('', { validators: [Validators.required] }),
+        number: new FormControl('', { validators: [Validators.required] }),
 
-      street: new FormControl('', { validators: [Validators.required] }),
+        postalCode: new FormControl('', { validators: [Validators.required] }),
 
-      number: new FormControl('', { validators: [Validators.required] }),
-
-      postalCode: new FormControl('', { validators: [Validators.required] }),
-
-      city: new FormControl('', { validators: [Validators.required] }),
+        city: new FormControl('', { validators: [Validators.required] }),
+      }),
 
       role: new FormControl<Role>('student', {
         validators: [Validators.required],
